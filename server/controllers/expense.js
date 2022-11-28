f02a4702dcff0e6ecfed610a4af63f23a74c40cd
nanaobeng <nanaobengmarnu@gmail.com>
2022-11-28 09:39:26 +0000

    Expense Controllers

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -162,3 +162,194 @@ exports.createExpenseCategory = async (req, res) => {
     }
   };
   
+
+//   Expenses
+
+
+exports.createExpense = async (req, res) => {
+
+    try {
+      let form = new formidable.IncomingForm();
+      form.parse(req, async (err, fields) => {
+      const { title, category , cost , description } = fields;
+
+      const currentYear =  (new Date()).getFullYear()
+      const currentMonth = ((new Date()).getMonth() + 1 )
+     
+      const newRequest = await Expense.findOne({ where: { title: `${title}` , month : `${currentMonth}` , year : `${currentYear}`} });
+  
+      if (newRequest) {
+        return res.status(401).json(
+            {
+                success: false,
+                msg : "Expense already exists!"
+            }
+            
+            );
+      }
+      const newExpense = await Expense.create({
+        title: title,
+        category: category,
+        cost : cost ,
+        description : description,
+        month : currentMonth ,
+        year : currentYear
+
+       
+      });
+  
+    
+      return res.json({ success: true ,
+    msg : "Expense Successfully Created"});
+      })
+      
+    
+  
+      
+  
+    
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send( {
+        success: false,
+        msg : "Error occured during creation."
+    });
+    }
+  };
+
+
+  exports.getExpense = async (req, res) => {
+    try {
+    
+      const { id } = req.params;
+  
+  
+  
+      const expense = await Expense.findOne({ where: { id: id } });
+  
+      if (expense) {
+        return res.status(200).json({success:true,data:expense});
+  
+   
+      } else {
+        return res.status(401).json({ success:false,error: `Expense not Found!` });
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
+
+
+  exports.getExpenses = async (req, res) => {
+    try {
+    
+    
+  
+  
+  
+      const expenses = await Expense.findAll();
+  
+      if (expenses) {
+        return res.status(200).json({success:true,data:expenses});
+  
+   
+      } else {
+        return res.status(401).json({success:false,error:'No Expenses Found'});
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  exports.getExpensesByCategory = async (req, res) => {
+    try {
+    
+      const { category_id } = req.params;
+  
+  
+  
+      const expenses = await Expense.findAll({ where: { category: category_id } });
+  
+      if (expenses) {
+        return res.status(200).json({success:true,data:expenses});
+  
+   
+      } else {
+        return res.status(401).json({ success:false,error: `Expenses not Found!` });
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
+
+
+  exports.updateExpense = async (req, res) => {
+    try {
+    
+  
+    
+  
+      let form = new formidable.IncomingForm();
+      form.parse(req, async (err, fields) => {
+      const { title, category , cost , description ,id } = fields;
+      
+      const expense = await Expense.findOne({ where: { id: id } });
+      if (!expense) {
+        return res.status(401).json({success:false,msg:'Expense not found'});
+      }
+      await Expense.update(
+        {
+            title: title,
+            category: category,
+            cost : cost ,
+            description : description,
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+  
+      res.json({success:true,msg:"Expense succesfully updated!"});
+      })
+  
+   
+  
+    
+  
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
+  
+  
+  exports.deleteExpense = async (req, res) => {
+    try {
+    
+
+        const { id } = req.body;
+  
+  
+  
+      const expense = await Expense.findOne({ where: { id: id } });
+  
+      if (expense) {
+        await Expense.destroy({
+          where: {
+            id: id,
+          },
+        });
+  
+        res.json({success:true,msg:"Expense succesfully deleted!"});
+      } else {
+        return res.status(401).json({ success: false ,error: `Expense  not Found!` });
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
