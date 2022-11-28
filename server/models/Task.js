96a4afc42f7ef5f59e5045d76fe97e7285fd88e4
nanaobeng <nanaobengmarnu@gmail.com>
2022-11-28 11:47:15 +0000

    Task Model

---
+++ server/models/Task.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/db");
+
+const Task =  sequelize.define(
+    "Task",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      recipient: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      proposed_deadline: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+
+
+      
+
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Task",
+    }
+  );
+
+  module.exports = Task;
