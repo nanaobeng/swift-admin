67e3be3a74fd1044d5ef21439fd272b0758480e6
nanaobeng <nanaobengmarnu@gmail.com>
2022-11-29 09:40:37 +0000

    Property Model

---
+++ server/models/Property.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/db");
+
+const Property =  sequelize.define(
+    "Property",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      developer: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+    
+      },
+      purchase_type: {
+        type: DataTypes.STRING,
+       
+      },
+      location: {
+        type: DataTypes.STRING,
+      
+      },
+      property_type: {
+        type: DataTypes.STRING,
+      },
+      visits: {
+        type: DataTypes.INTEGER
+      },
+      rooms: {
+        type: DataTypes.INTEGER
+      },
+      price: {
+        type: DataTypes.FLOAT
+      },
+      remarks: {
+        type: DataTypes.TEXT
+      },
+      status: {
+        type: DataTypes.STRING
+      },
+
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Property",
+    }
+  );
+
+  module.exports = Property;
